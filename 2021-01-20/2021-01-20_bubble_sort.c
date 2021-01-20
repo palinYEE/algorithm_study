d34ba16d5863dd29a9f99f5b6b4f6ad5194e08c7
윤영진(학생-정보보안암호수학과) <alwns28@kookmin.kr>
2021-01-20 22:58:46 +0900

    2021-01-20

---
+++ 2021-01-20/2021-01-20_bubble_sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<time.h>
+
+int main(){
+    int i, j, temp;
+    clock_t start_time, end_time;
+    int array[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+    printf("[*] Lets sort array {1, 10, 5, 8, 7, 6, 4, 3, 2, 9} by using Bubble Sort \n");
+    start_time = clock();
+    for(int k=0; k<100000000; k++){
+        for(i=0; i<10; i++){
+            for(j=0; j<9-i; j++){
+                if(array[j] > array[j+1]){
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+    end_time = clock();
+    printf("[*] Bubble Sort Result { ");
+    for(i=0; i<10; i++){
+        printf("%d, ", array[i]);
+    }
+    printf("}\n");
+
+
+    printf("[*] total elapsed time : %d", (end_time-start_time)/1000);
+    
+}
